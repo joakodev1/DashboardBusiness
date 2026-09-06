81b633996d445df09cc48a5f963945bbd8540f68
joakodev1 <notjoaco@gmail.com>
2026-09-06 19:56:04 -0300

    Agrego gunicorn y configuro ALLOWED_HOSTS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-b+aj@y!kwrxjhgen5k7$2xr4@&rd=zp05=w@7@7s4kb-@c9-kz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
